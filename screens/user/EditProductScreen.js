6ecade3253c67eaa6e11593815a9795ee9d90d60
bdmostafa <mdmostafaalmahmud@gmail.com>
2022-01-01 23:37:45 +0600

    configures textInput with validation

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -6,6 +6,7 @@ import {
   TextInput,
   StyleSheet,
   Platform,
+  Alert,
 } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { useSelector, useDispatch } from "react-redux";
@@ -20,6 +21,7 @@ export const EditProductScreen = ({ navigation }) => {
     state.products.userProducts.find((product) => product.id === productId)
   );
 
+  const [titleIsValid, setTitleIsValid] = useState(false);
   const [title, setTitle] = useState(editedProduct ? editedProduct.title : "");
   const [imageUrl, setImageUrl] = useState(
     editedProduct ? editedProduct.imageUrl : ""
@@ -31,6 +33,13 @@ export const EditProductScreen = ({ navigation }) => {
 
   const submitHandler = useCallback(() => {
     // console.log("Submitting!!!")
+    if (!titleIsValid) {
+      Alert.alert("Wrong input!", "Please check the errors in the form.", [
+        { text: "Okay" },
+      ]);
+      return;
+    }
+
     if (editedProduct) {
       dispatch(
         productActions.updateProduct(productId, title, description, imageUrl)
@@ -47,6 +56,15 @@ export const EditProductScreen = ({ navigation }) => {
     navigation.setParams({ submit: submitHandler });
   }, [submitHandler]);
 
+  const titleChangeHandler = (text) => {
+    if (text.trim().length === 0) {
+      setTitleIsValid(false);
+    } else {
+      setTitleIsValid(true);
+    }
+    setTitle(text);
+  };
+
   return (
     <ScrollView>
       <View style={styles.form}>
@@ -56,7 +74,16 @@ export const EditProductScreen = ({ navigation }) => {
             style={styles.input}
             value={title}
             onChangeText={(text) => setTitle(text)}
+            keyboardType="default"
+            autoCapitalize="words"
+            autoCorrect
+            returnKeyType="next"
+            onEndEditing={() => console.log("onEndEditting!!!")}
+            onSubmitEditing={() => console.log("onSubmitEditing")}
           />
+          {!titleIsValid && (
+            <Text style={styles.errorMsg}>Please enter a valid title!</Text>
+          )}
         </View>
         <View style={styles.formControl}>
           <Text style={styles.label}>Image URL</Text>
@@ -73,6 +100,7 @@ export const EditProductScreen = ({ navigation }) => {
               style={styles.input}
               value={price}
               onChangeText={(text) => setPrice(text)}
+              keyboardType="decimal-pad"
             />
           </View>
         )}
@@ -126,5 +154,9 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     borderBottomColor: "#ccc",
     borderBottomWidth: 1,
+  },
+  errorMsg: {
+    fontStyle: "italic",
+    color: "red",
   },
 });
