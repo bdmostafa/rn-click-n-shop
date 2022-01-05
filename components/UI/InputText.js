bb63c39ef509daed163d2b47e2b4fa127881472d
bdmostafa <mdmostafaalmahmud@gmail.com>
2022-01-05 20:03:03 +0600

    separate inputtext component

---
+++ components/UI/InputText.js
@@ -0,0 +1,126 @@
+import React, { useReducer, useEffect } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+
+const INPUT_CHANGE = "INPUT_CHANGE";
+const INPUT_BLUR = "INPUT_BLUR";
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case INPUT_CHANGE:
+      return {
+        ...state,
+        value: action.value,
+        isValid: action.isValid,
+      };
+
+    case INPUT_BLUR:
+      return {
+        ...state,
+        touched: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export const InputText = (props) => {
+  const {
+    id,
+    label,
+    errorText,
+    initialValue,
+    initiallyValid,
+    onInputChange,
+    required,
+    min,
+    max,
+    minLength,
+    email,
+    number,
+  } = props;
+
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: initialValue ? initialValue : "",
+    isValid: initiallyValid,
+    touched: false,
+  });
+
+  const textChangeHandler = (text) => {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    let isValid = true;
+
+    if (required && text.trim().length === 0) {
+      isValid = false;
+    }
+
+    if (email && !emailRegex.test(text.toLowerCase())) {
+      isValid = false;
+    }
+
+    if (min != null && +text < min) {
+      isValid = false;
+    }
+
+    if (number != null) {
+      const IsNumeric = (text) => /^-{0,1}\d*\.{0,1}\d+$/.test(text);
+      isValid = IsNumeric(text);
+    }
+
+    if (max != null && +text > max) {
+      isValid = false;
+    }
+
+    if (minLength != null && text.length < minLength) {
+      isValid = false;
+    }
+
+    dispatch({ type: INPUT_CHANGE, value: text, isValid: isValid });
+  };
+
+  const lostFocusHandler = () => {
+    dispatch({ type: INPUT_BLUR });
+  };
+
+  useEffect(() => {
+    if (inputState.touched) {
+      onInputChange(id, inputState.value, inputState.isValid);
+    }
+  }, [id, onInputChange, inputState]);
+
+  return (
+    <View style={styles.formControl}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        {...props}
+        style={styles.input}
+        value={inputState.value}
+        onChangeText={textChangeHandler}
+        onBlur={lostFocusHandler}
+      />
+      {!inputState.isValid &&
+        inputState.touched && (<Text style={styles.errorMsg}>{errorText}</Text>)}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  formControl: {
+    width: "100%",
+  },
+  label: {
+    fontFamily: "open-sans-bold",
+    marginVertical: 8,
+  },
+  input: {
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  errorMsg: {
+    fontStyle: "italic",
+    color: "red",
+    fontFamily: "open-sans",
+  },
+});
